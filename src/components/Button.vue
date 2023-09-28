4d94ffae1d6cbd713d6d67bbe365a91d7c5a8066
Carlos Marques <psycarlo1@gmail.com>
2023-09-28 04:35:25 +0100

    Progress

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <a :href="to" class="shadow-md py-2 px-5 bg-brand-darkest text-brand-lightest rounded-lg font-medium text-sm hover:-translate-y-[2px] transition-transform duration-300">
+    {{ text }}
+  </a>
+</template>
+
+<script setup lang="ts">
+  import { type PropType } from 'vue';
+
+  type Variant = 'primary' | 'subtle'
+
+  defineProps({
+    variant: {
+      type: String as PropType<Variant>,
+      required: true
+    },
+    to: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  })
+</script>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          black: '#000000',
+          darkest: '#161616',
+          darker: '#212121',
+          darky: '#2B2B2B',
+          dark: '#373737',
+          gray: '#9A9CA4',
+          grayDark: '#666D80',
+          light: '#EBEEF3',
+          lighty: '#F1F4F8',
+          lighter: '#F6F8FA',
+          lightest: '#FFFFFF',
+          white: '#FFFFFF',
+          success: '#28C780',
+        },
+      },
+      fontFamily: {
+        brand: ['Inter', 'sans-serif']
+      },
+    },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [],
 };
