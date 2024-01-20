28181845ae1afb607b5236a3b36a1ebd8919ebef
Carlos Marques <psycarlo1@gmail.com>
2024-01-20 22:56:16 +0000

    chore: add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vue from '@astrojs/vue'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  output: "server",
-  adapter: vercel()
-});
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
+})
