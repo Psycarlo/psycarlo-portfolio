e1ea131f9b97c06d05d308e6b8b345b2b9cb0a6b
Carlos Marques <psycarlo1@gmail.com>
2024-04-06 16:41:09 +0100

    chore: add expo

--- src/components/TechnologiesCard.vue
+++ src/components/TechnologiesCard.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="dark:border-brand-grayDarkest flex flex-col rounded-lg border border-brand-light bg-brand-lightest p-4 shadow-sm dark:bg-brand-dark"
+    class="flex flex-col rounded-lg border border-brand-light bg-brand-lightest p-4 shadow-sm dark:border-brand-grayDarkest dark:bg-brand-dark"
   >
     <div class="flex items-center justify-between">
       <p class="text-lg font-semibold">{{ title }}</p>
       <slot name="icon"></slot>
     </div>
     <p class="text-xs text-brand-gray">{{ description }}</p>
-    <div class="mt-3 grid grid-cols-3 xs:grid-cols-5 gap-2 w-fit">
+    <div class="mt-3 grid w-fit grid-cols-3 gap-2 xs:grid-cols-6">
       <slot></slot>
     </div>
   </div>
