530a3ce147207b37733bfb38a560a01c81a5aeae
psycarlo <psycarlo1@gmail.com>
2025-10-02 15:39:58 +0100

    fix: astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
 
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 
 export default defineConfig({
   integrations: [tailwind(), vue()],
